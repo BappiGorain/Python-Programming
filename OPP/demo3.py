aba8c5fd07ab236f86193bdbae35424466c593a1
Bappi Gorain <146017226+BappiGorain@users.noreply.github.com>
2025-01-22 17:47:09 +0530

    Create demo3.py

---
+++ OPP/demo3.py
@@ -0,0 +1,10 @@
+class Student:
+    def __init__(self,name,age,mark): 
+        self.name = name
+        self.age = age
+        self.mark = mark
+        
+s1=Student('Bappi',21,94)
+print('hello i am :',s1.name)
+print('my age is :', s1.age)
+print('My mark is :',s1.mark)
